3ec71300591e3150d34f6e7c783e67ae95799520
Author JSmart <author.japps@gmail.com>
2019-04-27 17:20:43 +0100

    ISS-226 # Load Junit5 Jupiter Tests - Green - More examples follows

---
+++ src/test/java/org/jsmart/zerocode/samplesjunit5/loadjupiter/JUnit5LoadTest.java
@@ -0,0 +1,24 @@
+package org.jsmart.zerocode.samplesjunit5.loadjupiter;
+
+import org.jsmart.zerocode.core.domain.LoadWith;
+import org.jsmart.zerocode.core.domain.TestMapping;
+import org.jsmart.zerocode.core.domain.TestMappings;
+import org.jsmart.zerocode.jupiter.extension.ZeroCodeParallelExtension;
+import org.jsmart.zerocode.samplesjunit5.jupiter.JUnit5Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith({ZeroCodeParallelExtension.class})
+public class JUnit5LoadTest {
+
+    @Test
+    @LoadWith("load_generation.properties")
+    @TestMappings({
+            @TestMapping(testClass = JUnit5Test.class, testMethod = "testX"),
+            @TestMapping(testClass = JUnit5Test.class, testMethod = "testY")
+    })
+    public void testLoad() {
+        //Remains empty
+    }
+
+}
